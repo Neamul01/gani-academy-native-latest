5c1d8b9a656a7906861d20d49c0418214d90e241
Neamul01 <neamulhaque2002@gmail.com>
2023-12-26 14:52:56 +0600

    chat feature added

---
+++ config/firebase.ts
@@ -0,0 +1,33 @@
+import { initializeApp } from "firebase/app";
+import Constants from "expo-constants";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const apiKey = Constants.expoConfig?.extra?.apiKey;
+const authDomain = Constants.expoConfig?.extra?.authDomain;
+
+const projectId = Constants.expoConfig?.extra?.projectId;
+const storageBucket = Constants.expoConfig?.extra?.storageBucket;
+
+const messagingSenderId = Constants.expoConfig?.extra?.messagingSenderId;
+const appId = Constants.expoConfig?.extra?.appId;
+
+const firebaseConfig = {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const database = getFirestore(app);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
+
+// export const auth = getAuth();
+// export const database = getFirestore();
---
+++ screens/Chat.tsx
@@ -0,0 +1,81 @@
+import React, {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useState,
+} from "react";
+import { GiftedChat } from "react-native-gifted-chat";
+import {
+  collection,
+  addDoc,
+  query,
+  orderBy,
+  onSnapshot,
+} from "firebase/firestore";
+import { auth, database } from "../config/firebase";
+
+interface IUser {
+  _id: string;
+  name: string;
+  avatar: string;
+}
+
+interface IMessage {
+  _id: string;
+  text: string;
+  createdAt: Date;
+  user: IUser;
+}
+
+export default function Chat() {
+  const [messages, setMessages] = useState<IMessage[]>([]);
+
+  useLayoutEffect(() => {
+    const collectionRef = collection(database, "chats");
+    const q = query(collectionRef, orderBy("createdAt", "desc"));
+
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      setMessages(
+        snapshot.docs.map((doc) => ({
+          _id: doc.id,
+          createdAt: doc.data().createdAt.toDate(),
+          text: doc.data().text,
+          user: doc.data().user as IUser,
+        }))
+      );
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  const onSend = useCallback((messages: IMessage[] = []) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, messages)
+    );
+
+    const { _id, createdAt, text, user } = messages[0];
+    addDoc(collection(database, "chats"), {
+      _id,
+      createdAt,
+      text,
+      user,
+    });
+  }, []);
+
+  return (
+    <GiftedChat
+      messages={messages}
+      onSend={(messages) => onSend(messages as IMessage[])}
+      messagesContainerStyle={{
+        backgroundColor: "#fff",
+      }}
+      imageStyle={{
+        borderWidth: 1,
+      }}
+      user={{
+        _id: auth.currentUser?.email || "unknown",
+        avatar: "https://i.pravatar.cc/300",
+      }}
+    />
+  );
+}
