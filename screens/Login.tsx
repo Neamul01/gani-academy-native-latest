03ddac6c367cd22915f3714cef889b1e2c7c3b7c
Neamul01 <neamulhaque2002@gmail.com>
2024-01-08 11:03:40 +0600

    google services

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -30,9 +30,10 @@ export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const navigation = useNavigation<any>();
-  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
+  const [request, response, promptAsync] = Google.useAuthRequest({
     androidClientId: ANDROID_CLIENT_ID,
   });
+  // const [request, response, promptAsync] = Google.useAuthRequest({ expoClientId: config.expoClientId});
 
   const onHandleLogin = () => {
     if (email !== "" && password !== "") {
